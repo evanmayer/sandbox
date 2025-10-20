9b040d1c0bac5b5f5c9af15f7dacc4702547358e
Evan Mayer <evanmayer@uchicago.edu>
2025-10-20 01:38:23 -0700

    example 3 data

--- debugging_ex3.py
+++ debugging_ex3.py
@@ -1,5 +1,6 @@
 # reduce a mess of a bug to minimum working example.
 
-# this code loads a .csv of a small portion of one of the data table's I'm
-# working with, and does statistics on it. However, the summary plots are blank!
-# The data is full of NaNs, and the code doesn't deal with it correctly.
+# this code loads a .csv (into a pandas dataframe) of a small portion of one of
+# the data tables I'm working with, and does statistics on it. However, the
+# summary plots are blank! The data is full of NaNs, and the code doesn't deal
+# with it correctly.
