f1ff7f8111832f53fe94ad87db50ddddd3931f78
Evan Mayer <evanmayer@uchicago.edu>
2025-10-20 01:28:59 -0700

    example 2

--- debugging_ex2.py
+++ debugging_ex2.py
@@ -1,5 +1,37 @@
 # run a non-working code to gain experience debugging.
 
-# this code has an insidious bug - a logical statement that is buggy with a bad
-# variable name, and with a subtle bug that actually overwrites a built-in Python
-# object.
+def check_3string_sequence(seq):
+    '''
+    Check that all three items in the sequence are unique.
+
+    Parameters
+    ----------
+    seq: iterable
+        An iterable of 3 strings
+
+    Returns
+    -------
+    is_unique: bool
+        True for strings with 3 unique items, False otherwise
+    '''
+    # First, check to make sure that all items in the sequence are strings
+    for item in seq:
+        assert isinstance(item, str)
+    # Unpack arguments and check sequence
+    (s, s1, s2) = seq
+    is_unique = (s != s1 != s2)
+    return is_unique
+
+
+if __name__ == '__main__':
+    str = 'cat'
+    str1 = 'dog'
+    str2 = 'cow'
+    is_unique = check_3string_sequence((str, str1, str2))
+    print('Sequence 1 unique?', is_unique)
+
+    str3 = 'bat'
+    str4 = ''
+    str5 = 'bat'
+    is_unique = check_3string_sequence((str3, str4, str5))
+    print('Sequence 2 unique?', is_unique)
